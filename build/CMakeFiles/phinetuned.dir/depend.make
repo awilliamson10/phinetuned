24d75491d1fe71c572607e15784eb96024314e08
awilliamson10 <aw.williamson10@gmail.com>
2024-01-06 20:20:57 -0500

    working

---
+++ build/CMakeFiles/phinetuned.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for phinetuned.
+# This may be replaced when dependencies are built.
---
+++ build/src/CMakeFiles/ggml.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aw/projects/phinetuned/src/ggml-alloc.c" "src/CMakeFiles/ggml.dir/ggml-alloc.c.o" "gcc" "src/CMakeFiles/ggml.dir/ggml-alloc.c.o.d"
+  "/Users/aw/projects/phinetuned/src/ggml-backend.c" "src/CMakeFiles/ggml.dir/ggml-backend.c.o" "gcc" "src/CMakeFiles/ggml.dir/ggml-backend.c.o.d"
+  "/Users/aw/projects/phinetuned/src/ggml-quants.c" "src/CMakeFiles/ggml.dir/ggml-quants.c.o" "gcc" "src/CMakeFiles/ggml.dir/ggml-quants.c.o.d"
+  "/Users/aw/projects/phinetuned/src/ggml.c" "src/CMakeFiles/ggml.dir/ggml.c.o" "gcc" "src/CMakeFiles/ggml.dir/ggml.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
